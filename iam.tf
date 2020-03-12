35c5c5e2c8805af2990483e31f68dccef8cde963
reireias <reireias@gmail.com>
2020-03-12 14:53:58 +0900

    fix fmt

--- iam.tf
+++ iam.tf
@@ -6,7 +6,7 @@ resource "google_service_account" "billing_bucket" {
 resource "google_project_iam_binding" "billing_bucket" {
   project = data.google_project.project.project_id
   role    = "roles/storage.objectViewer"
-  members  = [
+  members = [
     "serviceAccount:${google_service_account.billing_bucket.email}",
   ]
 }
