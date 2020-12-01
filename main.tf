16ee889a2076ae6fbda70868912ac259fe7c5ecf
reireias <reireias@gmail.com>
2020-12-01 23:25:54 +0900

    upgrade terraform 0.13.5

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_version = "= 0.12.6"
-}
-
 provider "google" {
   version     = "2.12.0"
   project     = "master-243612"
   region      = "asia-northeast1"
-  credentials = "${file(".credentials/terraform.json")}"
+  credentials = file(".credentials/terraform.json")
 }
 
 data "google_client_config" "current" {}
